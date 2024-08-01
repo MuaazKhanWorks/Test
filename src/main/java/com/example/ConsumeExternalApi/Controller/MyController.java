4f2c19cd78f4286516d0b4bc8a68ad13f2e99332
Muaaz Khan <muaazk564@gmail.com>
2024-08-02 01:04:14 +0500

    Main

---
+++ src/main/java/com/example/ConsumeExternalApi/Controller/MyController.java
@@ -0,0 +1,29 @@
+package com.example.ConsumeExternalApi.Controller;
+
+import com.theokanning.openai.audio.CreateTranscriptionRequest;
+import com.theokanning.openai.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+
+@RestController
+public class MyController {
+
+    @Value("${openai.api.key}")
+    private String apiKey;
+
+    @GetMapping("/audio")
+    public String audio(@RequestParam String filePath){
+        OpenAiService service = new OpenAiService(apiKey);
+
+        CreateTranscriptionRequest request = new CreateTranscriptionRequest();
+        request.setModel("whisper-1");
+        File file = new File(filePath);
+        String transcription = service.createTranscription(request,filePath).getText();
+        return transcription;
+    }
+
+}
---
+++ src/test/java/com/example/ConsumeExternalApi/ConsumeExternalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ConsumeExternalApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumeExternalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
